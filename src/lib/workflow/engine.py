1e88fed0a06748688b0b4e73c81661cd6f400b3e
Rob Howard <rob@zinclusive.com>
2026-02-01 12:32:47 -0700

    Workflow Engine MVP done.

---
+++ src/lib/workflow/engine.py
@@ -0,0 +1,4 @@
+"""
+Begin with an Object Model called config, which is a YAML or JSON file parsed into a Python dictionary.
+The structure defines the workflow, which is a graph of states and transitions between them.
+"""
